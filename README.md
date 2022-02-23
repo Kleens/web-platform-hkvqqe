c4c3aa9eb7e502a23d3087011b5d7b782c16cced
Kleens <kevensu7@gmail.com>
2022-02-23 16:51:01 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# web-platform-hkvqqe
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-platform-hkvqqe)
---
+++ index.js
@@ -0,0 +1,9 @@
+function linkone(linkino) {
+  window.open(linkino);
+}
+function sopra(xyz) {
+  document.getElementById(xyz).style.backgroundColor = '#e0e0e0';
+}
+function nonsopra(xyz) {
+  document.getElementById(xyz).style.backgroundColor = 'white';
+}
